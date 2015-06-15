91d72845a0f867f35cd8865b8d18a6bd0b4486de
briem-bixly <briem@bixly.com>
2015-06-15 15:04:36 -0700

    Update add_department.py

--- scripts/add_department.py
+++ scripts/add_department.py
@@ -2,12 +2,12 @@ from api.organization_management import add_department
 
 # NOTE: this script expects a trigger and a department name
 
-class add_department(NebriOS):
-    listens_to = ['add_department']
+class add_dept(NebriOS):
+    listens_to = ['add_dept']
 
     def check(self):
-        return self.add_department == True
+        return self.add_dept == True
 
     def action(self):
-        self.add_department = "Ran"
+        self.add_dept = "Ran"
         add_department(self)
